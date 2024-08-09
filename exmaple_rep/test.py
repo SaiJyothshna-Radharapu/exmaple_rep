2102a0c0638824841249d576fc51952224a46743
SaiJyothshna-Radharapu <saijyothshna.radharapu@adani.com>
2024-08-09 16:20:46 +0530

    new changes in test

--- exmaple_rep/test.py
+++ exmaple_rep/test.py
@@ -5,3 +5,10 @@ for i in range(0,7):
     for j in range(0,i+1):
         print("*",end=" ")
     print()
+
+
+
+for i in range(0,7):
+    for j in range(0,i+1):
+        print(":(", ":) ",end=" ")
+    print()
